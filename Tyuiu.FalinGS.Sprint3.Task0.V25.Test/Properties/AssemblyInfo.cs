60794dd869497983b5f4e8fcde515a886e9545e3
FalinGeorgiy <ivan.ivan.local@mail.ru>
2023-10-16 21:17:09 +0500

    Add project files.

---
+++ Tyuiu.FalinGS.Sprint3.Task0.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.FalinGS.Sprint3.Task0.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.FalinGS.Sprint3.Task0.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f8f7d7b3-b0bf-4f35-9c27-73fe0f6179d5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.FalinGS.Sprint3.Task0.V25/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FalinGS.Sprint3.Task0.V25.Lib;
+
+namespace Tyuiu.FalinGS.Sprint3.Task0.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Title = "Спринт #3 | Выполнил: Фалин Г.С | ИИПб-23-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Cпринт №3                                                               #");
+            Console.WriteLine("* Задание №0                                                              #");
+            Console.WriteLine("* Вариант №25                                                             #");
+            Console.WriteLine("* Выполнил: Фалин Георгий Сергеевич / ИИПб-23-1                           #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Исходные данные:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            DataService ds = new DataService();
+
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 14;
+
+            Console.WriteLine("Переменная Х =" + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = "+ stopValue);
+
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("* Result                                                                  #");
+            Console.WriteLine("###########################################################################");
+
+            Console.WriteLine("Сумма ряда =  " + ds.GetSumSeries(value,startValue,stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
