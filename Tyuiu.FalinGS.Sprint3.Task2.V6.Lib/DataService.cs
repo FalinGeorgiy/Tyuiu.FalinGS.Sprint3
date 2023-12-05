e9392f6852f9317160fe5dd99f690f36d5f9421d
User <falin.super@gmail.com>
2023-12-05 20:48:28 +0300

    Task 2,3

---
+++ Tyuiu.FalinGS.Sprint3.Task2.V6.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.FalinGS.Sprint3.Task2.V6.Lib
+{
+    public class DataService : ISprint3Task2V6
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double SumSeries = 0;
+            do
+            {
+                SumSeries = SumSeries + (Math.Pow(value, startValue) + 1) * (Math.Cos(4));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.FalinGS.Sprint3.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.FalinGS.Sprint3.Task2.V6.Lib;
+
+namespace Tyuiu.FalinGS.Sprint3.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = -6.754;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
